d397bbf728fe5803bfa6fa45b075cc490afa5fba
Nasurito <60777824+NathanGrng@users.noreply.github.com>
2020-09-02 16:44:49 +0200

    commit 2

---
+++ connectiondpdo.inc.php
@@ -0,0 +1,13 @@
+<?php
+$hostname = 'host=localhost';
+$username = 'root';
+$password = '';
+$bdd = 'bibilio';
+
+try {
+    $Pdo = new PDO("mysql:$hostname;dbname=$bdd;charset=utf8", $username, $password);
+    $Pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    $Pdo = null;
+}
---
+++ listeNationalites.php
@@ -0,0 +1,6 @@
+<?php include "./header.php";
+require_once "./connectiondpdo.inc.php";
+?>
+<main role="main">
+</main>
+<?php include "./footer.php"; ?>
